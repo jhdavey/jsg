a8e12d879a6d949f9d1572527ecaed2b783a0946
jhdavey <jhdavey5182@gmail.com>
2023-08-01 21:08:55 -0400

    seeds working again!

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,13 +1,20 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Location } = require('../models');
 
 const userData = require('./userData.json');
+const locationData = require('./locationData.json');
+
 
 db.once('open', async () => {
-  await User.deleteMany({});
 
-  const users = await User.insertMany(userData);
+    await User.deleteMany({});
+    await Location.deleteMany({});
 
-  console.log('Users seeded!');
-  process.exit(0);
-});
+    const users = await User.insertMany(userData);
+    const locations = await Location.insertMany(locationData);
+
+    console.log('Users and Locations seeded!');
+    process.exit(0);
+  });
+  
+  
