46a7bf12577627c1db5a8f21f24cf3761d5e6e7d
floretdemola <floretdemola@gmail.com>
2023-08-01 21:18:31 -0400

    typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,10 +4,47 @@ const typeDefs = gql`
   type User {
     _id: ID!
     name: String!
+    email: String!
+    password: String!
+    savedLocation: [Location]
+  }
+  
+  type Location {
+    locationId: ID!
+    name: String!
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input LocationInput {
+    locationId: ID!
+    name: String!
+    description: String!
+    image: String
+    link: String
+    title: String!
   }
 
   type Query {
     user: [User]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveLocation(locationData: LocationInput!): User
+    removeLocation(locationId: ID!): User
+  }
+  schema {
+    query: Query
+    mutation: Mutation
   }
 `;
 
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -7,25 +7,10 @@ const locationData = require('./locationData.json');
 
 db.once('open', async () => {
   await User.deleteMany({});
+  await Location.deleteMany({});
 
-  try {
-    await User.deleteMany({});
-    await Location.deleteMany({});
-    await User.create(userData);
-
-    for (let i = 0; i < locationData.length; i++) {
-      const { _id, title } = await Location.create(locationData[i]);
-      const user = await User.findOneAndUpdate(
-        { username: title },
-        {
-          $addToSet: { savedLocations: _id },
-        }
-      );
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+  const user = await User.insertMany(userData);
+  const locations = await Location.insertMany(locationData);
 
   console.log('Users seeded!');
   process.exit(0);
