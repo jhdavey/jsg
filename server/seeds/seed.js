569a842069f3205b220646b69607e3adbae116bc
floretdemola <floretdemola@gmail.com>
2023-08-01 20:36:21 -0400

    SEEDS WORK HFS

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,31 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Location } = require('../models');
 
 const userData = require('./userData.json');
+const locationData = require('./locationData.json');
+
 
 db.once('open', async () => {
   await User.deleteMany({});
 
-  const users = await User.insertMany(userData);
+  try {
+    await User.deleteMany({});
+    await Location.deleteMany({});
+    await User.create(userData);
+
+    for (let i = 0; i < locationData.length; i++) {
+      const { _id, title } = await Location.create(locationData[i]);
+      const user = await User.findOneAndUpdate(
+        { username: title },
+        {
+          $addToSet: { savedLocations: _id },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
   console.log('Users seeded!');
   process.exit(0);
