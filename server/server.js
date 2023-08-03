7047049dc1c917f8d8947b57c72f5626c0583389
jhdavey <jhdavey5182@gmail.com>
2023-08-03 19:07:25 -0400

    Installed tailwindcss

--- server/server.js
+++ server/server.js
@@ -1,47 +1,48 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Configuration, OpenAIApi} = require("openai");
 const { graphqlHTTP } = require('express-graphql');
 const  mongoose = require("mongoose");
-const { typeDefs, resolvers } = require("./schema");
+const { typeDefs, resolvers } = require("./schemas");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const bodyParser = require("body-parser");
+
+//Setup Open Ai connection
+const config = new Configuration({
+  apiKey: "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"
+})
+
+const openai = new OpenAIApi(config);
 
 //Initialize instance of express server
 const app = express();
-
-//Set up OpenAI connection
-const { Configuration, OpenAIApi } = require("openai");
-const config = new Configuration({
-  //TODO - WILL NEED TO MOVE KEY TO PROTECT FROM PUBLIC
-  apiKey: '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',
-});
-const openai = new OpenAIApi(config);
-
-//OpenAI Middleware required
 app.use(bodyParser.json());
 app.use(cors());
 
-//Set up OpenAI post route - Can move to routes later if needed
 app.post("/chat", async (req, res) => {
   const {prompt} = req.body;
-  const completion = await openai.createCompletion({
+  
+  const completion = openai.createCompletion({
     model: "text-davinci-003",
+    messages: [{"role": "system",
+              "content": "You are a travel assistant that provides recommendations for activities and restaurants based on destination provided by the user."
+  }],
     max_tokens: 512,
     temperature: 0,
     prompt: prompt,
   });
-  res.send(completion.data.choices[0].text);
-});
+  res.send((await completion).data.choices[0].text);
+})
 
+app.use(express.json());
 
 // import user defined files
 const { authMiddleware } = require('./utils/auth');
 
 const db = require("./config/connection");
 
-// express server
-const PORT = process.env.PORT || 3001;
+
 // instance of Apollo server. pass in schema data
 const server = new ApolloServer({
   typeDefs,
@@ -51,7 +52,6 @@ const server = new ApolloServer({
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 // serve client/build as static assets while in production
 if (process.env.NODE_ENV === "production") {
@@ -66,6 +66,8 @@ app.get("/", (req, res) => {
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  const PORT = 3001;
 
   db.once("open", () => {
     app.listen(PORT, () => {
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import ChatGPT from "../ChatGPT"
 export default function Home() {
     return (
         <div>
-            <h1>Jet Set Go!</h1>
+            <h1 className="text-red-500">Jet Set Go!</h1>
             <ChatGPT />
         </div>
     )
