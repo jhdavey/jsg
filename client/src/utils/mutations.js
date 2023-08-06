39f9b2d4b140b1b06a0ca354c0ad9377e2a96538
jhdavey <jhdavey5182@gmail.com>
2023-08-06 17:38:20 -0400

    Sign up working, but stll showing error in UI.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,27 @@
 const { gql } = require('@apollo/client');
 
+export const ADD_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
+      password
+      trips {
+        _id
+        destination
+        activities {
+          _id
+          activityName
+        }
+      }
+    }
+  }
+`;
+
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      token
       user {
         _id
         username
@@ -12,48 +30,4 @@ export const LOGIN_USER = gql`
   }
 `;
 
-export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
-
-export const SAVE_LOCATION = gql`
-  mutation saveLocation($locationData: LocationInput!) {
-    saveLocation(locationData: $locationData) {
-      _id
-      username
-      email
-      saveLocation {
-        locationId
-        username
-        description
-        image
-        link
-      }
-    }
-  }
-`;
-
-export const REMOVE_LOCATION = gql`
-  mutation removeLocation($locationId: ID!) {
-    removeLocation(locationId: $locationId) {
-      _id
-      username
-      email
-      saveLocation {
-        locationId
-        username
-        description
-        image
-        link
-      }
-    }
-  }
-`;
+//ADD TRIPS AND ACTIVITIES
