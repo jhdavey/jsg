a2b4a38ae9a2e5609ca4fa4761f34e0a7da2a3a1
jhdavey <jhdavey5182@gmail.com>
2023-08-08 22:38:21 -0400

    server updated again

--- server/server.js
+++ server/server.js
@@ -26,6 +26,14 @@ const config = new Configuration({
 
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "../client"));
+});
+
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
 };
 
